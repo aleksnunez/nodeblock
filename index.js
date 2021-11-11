d03996d9d7e2c950e5372d230ddee54e323e1c39
Alex Nunez <nunezaleks@gmail.com>
2021-11-11 10:30:29 -0800

    add index.js and open package to run project from terminal

---
+++ index.js
@@ -0,0 +1,75 @@
+let CryptoJS = require("crypto-js/core");
+CryptoJS.SHA256 = require("crypto-js/sha256");
+
+const calculateHash = ({ previousHash, timestamp, data, nonce = 1 }) => 
+    CryptoJS.SHA256(previousHash + timestamp + JSON.stringify(data) + nonce).toString()
+
+const generateGenesisBlock = () => {
+    const block = {
+        previousHash: "0",
+        timestamp: + new Date(),
+        data: "Genesis Block",
+    };
+    return {
+        ...block,
+        hash: calculateHash(block)
+    }
+
+}
+
+const hashConstraints = (difficulty, hash) => 
+    hash.substr(0, difficulty) === "0".repeat(difficulty)
+
+const nextNonce = (block) => updateHash({...block, nonce: block.nonce + 1})
+
+const updateHash = (block) => { 
+    return { ...block, hash: calculateHash(block)}
+}
+
+
+const trampoline = (func) => {
+    let result = func.apply(func, ...arguments)
+    while(result && typeof(result) === "function") {
+        result = result()
+    }
+    return result
+}
+
+const mineBlock = (difficulty, block) => {
+    const mine = (block) => {
+        const newBlock = nextNonce(block)
+        return hashConstraints(difficulty, newBlock.hash)
+                ? newBlock
+                : () => mine(nextNonce(block))
+    }
+    return trampoline(mine(nextNonce(block)))
+}
+
+const addBlock = (chain, data) => {
+    const { hash: previousHash } = chain[chain.length - 1]
+    const block                  = { timestamp: + new Date(), data, previousHash, nonce: 0 }
+    const newBlock               = mineBlock(4, block)
+    return chain.concat(newBlock)
+
+}
+
+  let chain = [generateGenesisBlock()]
+
+  const newBlockData = {
+    sender:   "ks829fh28123jhrv9dk9",
+    receiver: "ads8d91w29j32er52910",
+    amount:   0.0023,
+    currency: "ETH"
+  }
+
+const newChain = addBlock(chain, newBlockData)
+
+
+console.log(generateGenesisBlock());
+console.log(newChain);
+
+const open = require('open');
+
+(async () => {
+        await open('index.html', {"wait": true });
+})();
