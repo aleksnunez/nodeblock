c15b94f12c34e56a083e5f8d299fcb17c5de5083
Alex Nunez <nunezaleks@gmail.com>
2021-11-11 11:19:19 -0800

    update to display newChain

--- bundle.js
+++ bundle.js
@@ -1,6 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-//let SHA256 = require("crypto-js/sha256");
-var CryptoJS = require("crypto-js/core");
+let CryptoJS = require("crypto-js/core");
 CryptoJS.SHA256 = require("crypto-js/sha256");
 
 const calculateHash = ({ previousHash, timestamp, data, nonce = 1 }) => 
@@ -66,8 +65,8 @@ const addBlock = (chain, data) => {
 
   const newChain = addBlock(chain, newBlockData)
 
-document.write(JSON.stringify(generateGenesisBlock()));
-console.log(generateGenesisBlock());
+document.write(JSON.stringify(newChain));
+
 
 },{"crypto-js/core":2,"crypto-js/sha256":3}],2:[function(require,module,exports){
 (function (global){(function (){
